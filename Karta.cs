b28daa2e26226479bc3c75e564dcb592e1f33e1f
kuadalma <94044289+kuadalma@users.noreply.github.com>
2023-09-25 14:57:54 +0200

    fdfgfdsgfdssadfdsafdsfdsfdsf

--- Karta.cs
+++ Karta.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Poker_dobierany
+﻿namespace Poker_dobierany
 {
     internal class Karta
     {
         public int numer;
         public string kolor;
+        public string name;
         public Karta(int numer, string kolor)
         {
             this.numer = numer;
             this.kolor = kolor;
+            switch (numer)
+            {
+                case 10:
+                    name = "T";
+                    break;
+                case 11:
+                    name = "J";
+                    break;
+                case 12:
+                    name = "Q";
+                    break;
+                case 13:
+                    name = "K";
+                    break;
+                case 14:
+                    name = "A";
+                    break;
+                default:
+                    name = numer.ToString();    
+                    break;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{name}{kolor}";
         }
     }
 }
